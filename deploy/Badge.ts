2e4fb08dd1dd99e04c9b11fe5ce962dc1bf3b6bb
Kantapat Chankasem <kantapatchankasem@Kantapats-MacBook-Pro.local>
2022-10-08 14:32:05 +0700

    add: badge contract and deploy script

--- deploy/Badge.ts
+++ deploy/Badge.ts
@@ -2,7 +2,34 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction, DeployResult } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  // TODO: Add deploy script
+  const { deployments, getNamedAccounts, network } = hre
+  const { deployer } = await getNamedAccounts()
+
+  const { deploy } = deployments
+
+  const NAME = "Name"
+  const SYMBOL = "Symbol"
+  const BADGE_NAME = "Chainlink workshop #1"
+  const BADGE_DESCRIPTION = "First workshop event from chainlink"
+
+  const result: DeployResult = await deploy("Badge", {
+    contract: "Badge",
+    from: deployer,
+    args: [NAME, SYMBOL, BADGE_NAME, BADGE_DESCRIPTION],
+    log: true,
+    waitConfirmations: 6,
+  })
+
+  console.log("Result = ", result.address)
+
+  try {
+    await hre.run("verify:verify", {
+      address: result.address,
+      constructorArguments: [NAME, SYMBOL, BADGE_NAME, BADGE_DESCRIPTION],
+    })
+  } catch (err) {
+    console.log("Error: ", err)
+  }
 }
 
 export default func
